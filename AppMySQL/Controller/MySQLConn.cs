23388836c81ba577a8c639c9d5bf181ee8a2e221
Aluno <Aluno@SNCTM202LABF04>
2024-02-28 22:12:24 -0300

    Adicionar arquivos de projeto.

---
+++ AppMySQL/Controller/MySQLConn.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using MySqlConnector;
+using AppMySQL.Models;
+using System.Globalization;
+
+namespace AppMySQL.Controller
+{
+    public class MySQLConn
+    {
+        static string conn = @"server=sql.freedb.tech;port=3306;database=freedb_pietro_tds10;user id=freedb_pips96;password=Pv7NP7Y&qB%n!R3;charset=utf8";
+        public static List<Pessoas> ListarPessoas()
+        {
+            List<Pessoas> listapessoas = new List<Pessoas>();
+            string sql = "SELECT * FROM pessoa";
+            using (MySqlConnection con = new MySqlConnection(conn))
+            {
+                con.Open();
+                using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Pessoas pessoa = new Pessoas()
+                            {
+                                id = reader.GetInt32(0),
+                                nome = reader.GetString(1),
+                                idade = reader.GetString(2),
+                                cidade = reader.GetString(3)
+                            };
+                            listapessoas.Add(pessoa);
+                        }
+                    }
+                }
+                con.Close();
+                return listapessoas;
+            }
+        }
+
+        public static void InserirPessoa(string nome,string idade,string cidade)
+        {
+            string sql = "INSERT INTO pessoa(nome,idade,cidade) VALUES (@nome,@idade,@cidade)";
+        }
+    }
+}
---
+++ AppMySQL/Models/Pessoas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace AppMySQL.Models
+{
+    public class Pessoas
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string idade { get; set; }
+        public string cidade {get; set; }
+
+    }
+}
